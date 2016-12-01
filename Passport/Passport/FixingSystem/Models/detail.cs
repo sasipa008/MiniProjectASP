9c07a560732374dd0766a79d24eb7838452147e1
Thummarong085 <bercup_1995@live.com>
2016-12-01 23:33:47 +0700

    Add fixing System

---
+++ Passport/Passport/FixingSystem/Models/detail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FixingSystem.Models
+{
+    public class detail
+    {
+        public int id { get; set; }
+        public string Building { get; set; }
+        public int Floor { get; set; }
+        public string Room { get; set; }
+        public string Details { get; set; }
+    }
+}
